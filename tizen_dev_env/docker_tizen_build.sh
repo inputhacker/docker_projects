12dd351f6b09ae6e8e4027c16bfcdcd5da71f640
Sung-Jin Park <sj76.park@samsung.com>
2017-05-30 13:11:46 +0900

    tizen_dev_env : fix Dockerfile to add ssh keys into a user's .ssh directory
    Signed-off-by: Sung-Jin Park <sj76.park@samsung.com>

--- tizen_dev_env/docker_tizen_build.sh
+++ tizen_dev_env/docker_tizen_build.sh
@@ -7,39 +7,48 @@ function usage
 	exit
 }
 
-if ["$1" = ""]; then
+echo "user=${USER}, home=${HOME}"
+
+if [ "$1" = "" ]; then
 	usage
 fi
 
 TAG="$1"
-if ["$TAG" = ""]; then
+if [ "$TAG" = "" ]; then
 	TAG="ubuntu_tizen"
 fi
 
 WORK_DIR="$2"
-if ["$WORK_DIR" = ""]; then
+if [ "$WORK_DIR" = "" ]; then
 	WORK_DIR="."
 fi
 
 GIT_USER_NAME="$3"
-if ["$GIT_USER_NAME" = ""]; then
+if [ "$GIT_USER_NAME" = "" ]; then
 	usage
 fi
 
 GIT_EMAIL="$4"
-if ["$GIT_EMAIL" = ""]; then
+if [ "$GIT_EMAIL" = "" ]; then
 	usage
 fi
 
 TIZEN_SSH_USER_ID="$5"
-if ["$TIZEN_SSH_USER_ID" = ""]; then
+if [ "$TIZEN_SSH_USER_ID" = "" ]; then
 	usage
 fi
 
 TIZEN_SSH_EMAIL="$6"
-if ["$TIZEN_SSH_EMAIL" = ""]; then
+if [ "$TIZEN_SSH_EMAIL" = "" ]; then
 	usage
 fi
 
-docker build --build-arg GIT_USER_NAME="${GIT_USER_NAME}" --build-arg GIT_EMAIL="${GIT_EMAIL}" --build-arg TIZEN_SSH_USER_ID="${TIZEN_SSH_USER_ID}" --build-arg TIZEN_SSH_EMAIL="${TIZEN_SSH_EMAIL}"  --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg USER=$USER --build-arg UID=$UID --build-arg SHELL=$SHELL --no-cache=false -t ${TAG} ${WORK_DIR}
-#docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg USER=$USER --build-arg UID=$UID --build-arg SHELL=$SHELL --no-cache=true -t ${TAG} ${WORK_DIR}
+cp -af ${HOME}/.ssh .
+
+#use of cache
+docker build --build-arg GIT_USER_NAME="${GIT_USER_NAME}" --build-arg GIT_EMAIL="${GIT_EMAIL}" --build-arg TIZEN_SSH_USER_ID="${TIZEN_SSH_USER_ID}" --build-arg TIZEN_SSH_EMAIL="${TIZEN_SSH_EMAIL}"  --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg USER=$USER --build-arg UID=$UID --build-arg SHELL=$SHELL --build-arg HOME_DIR="${HOME}" --no-cache=false -t ${TAG} ${WORK_DIR}
+
+#no use of cache
+#docker build --build-arg GIT_USER_NAME="${GIT_USER_NAME}" --build-arg GIT_EMAIL="${GIT_EMAIL}" --build-arg TIZEN_SSH_USER_ID="${TIZEN_SSH_USER_ID}" --build-arg TIZEN_SSH_EMAIL="${TIZEN_SSH_EMAIL}"  --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --build-arg USER=$USER --build-arg UID=$UID --build-arg SHELL=$SHELL --build-arg HOME_DIR="${HOME}" --no-cache=true -t ${TAG} ${WORK_DIR}
+
+rm -rf .ssh
